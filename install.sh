fef0858ddc6754e880f77b700145b68960700f84
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-02-02 12:38:27 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Checking For Root User...."
+sleep 1
+if [[ $(id -u) -ne 0 ]] ; then 
+   echo "You are Not Root! Please Run as root" ; exit 1 ; 
+fi
+echo -e "\033[0;33m"
+echo "Executing apktool installer ..."
+cp -r apktool /usr/local/bin
+cp -r apktool /usr/local/sbin
+cp -r apktool.jar /usr/local/bin
+cp -r apktool.jar /usr/local/sbin
+chmod +x /usr/local/bin/apktool.jar
+chmod +x /usr/local/sbin/apktool.jar
+chmod +x /usr/local/bin/apktool
+chmod +x /usr/local/sbin/apktool
+echo "Installing apksigner ..."
+apt install apksigner -y
+echo "Installation Complete ..."
+sleep 1
+echo -e "Now you can able to\033[0;37m bind payloads\033[0;33m with any apk"
